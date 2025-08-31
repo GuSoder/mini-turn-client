8208caab61f1ff6f31f999db50e53a4de1a4633f
gus <gusoder@gmail.com>
2025-08-31 14:53:36 +0200

    Add game discovery and selection to lobby
    - Auto-fetch games on lobby startup
    - Press L to refresh game list
    - Press 1-N to select game, then 1-4 to join as player
    - Auto-select if only one game exists

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -3,54 +3,104 @@ extends Node3D
 var http_request: HTTPRequest
 var server_url: String = "http://207.154.222.143:5000"
 var current_game_id: String = ""
+var available_games: Array = []
 
 func _ready():
-	print("Lobby - Press C to create game, then 1-4 to join as player")
+	print("Lobby - Press C to create game, L to list games, then 1-4 to join as player")
 	
 	http_request = HTTPRequest.new()
 	add_child(http_request)
 	http_request.request_completed.connect(_on_request_completed)
+	
+	# Auto-load existing games on startup
+	list_games()
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		match event.keycode:
 			KEY_C:
 				create_game()
+			KEY_L:
+				list_games()
 			KEY_1:
-				join_as_player(1)
+				if available_games.size() >= 1:
+					select_game_or_join(1)
+				else:
+					join_as_player(1)
 			KEY_2:
-				join_as_player(2)
+				if available_games.size() >= 2:
+					select_game_or_join(2)
+				else:
+					join_as_player(2)
 			KEY_3:
-				join_as_player(3)
+				if available_games.size() >= 3:
+					select_game_or_join(3)
+				else:
+					join_as_player(3)
 			KEY_4:
-				join_as_player(4)
+				if available_games.size() >= 4:
+					select_game_or_join(4)
+				else:
+					join_as_player(4)
 
 func create_game():
 	print("Creating new game...")
 	var url = server_url + "/games"
 	http_request.request(url, [], HTTPClient.METHOD_POST)
 
+func list_games():
+	print("Fetching active games...")
+	var url = server_url + "/games"
+	http_request.request(url)
+
 func join_as_player(player_num: int):
 	if current_game_id == "":
-		print("No game created yet! Press C to create a game first.")
+		print("No game selected! Press L to list games or C to create one.")
 		return
 	
 	print("Joining as Player ", player_num)
 	var scene_path = "res://scenes/client" + str(player_num) + ".tscn"
 	
-	# Store game ID in global autoload or pass it somehow
-	# For now, we'll use a simple approach with a global variable
 	get_tree().set_meta("game_id", current_game_id)
 	get_tree().change_scene_to_file(scene_path)
+
+func select_game_or_join(num: int):
+	if current_game_id == "":
+		# Select game
+		if num <= available_games.size():
+			current_game_id = available_games[num - 1].gameId
+			print("Selected game: ", current_game_id)
+			print("Now press 1-4 to join as a player")
+	else:
+		# Join as player
+		join_as_player(num)
 
 func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray):
 	if response_code == 200:
 		var json = JSON.new()
 		var parse_result = json.parse(body.get_string_from_utf8())
 		
-		if parse_result == OK and "gameId" in json.data:
-			current_game_id = json.data.gameId
-			print("Game created! Game ID: ", current_game_id)
-			print("Now press 1, 2, 3, or 4 to join as a player")
+		if parse_result == OK:
+			if "gameId" in json.data:
+				# Game creation response
+				current_game_id = json.data.gameId
+				print("Game created! Game ID: ", current_game_id)
+				print("Now press 1, 2, 3, or 4 to join as a player")
+			elif "games" in json.data:
+				# Game list response
+				available_games = json.data.games
+				if available_games.size() == 0:
+					print("No active games found. Press C to create one.")
+					current_game_id = ""
+				else:
+					print("Active games:")
+					for i in range(available_games.size()):
+						var game = available_games[i]
+						print("  ", i+1, ": ", game.gameId, " (Turn: Player ", game.playerInTurn + 1, ")")
+					print("Press 1-", available_games.size(), " to select game, then 1-4 to join as player")
+					# Auto-select first game if only one exists
+					if available_games.size() == 1:
+						current_game_id = available_games[0].gameId
+						print("Auto-selected game: ", current_game_id)
 	else:
-		print("Failed to create game. Response code: ", response_code)
+		print("Server request failed. Response code: ", response_code)
